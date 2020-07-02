2d69238ad5f9bd0a28a268c6e7c5a0c72bc1a6ef
Endi S. Dewata <edewata@redhat.com>
2020-07-02 15:54:48 -0500

    Updated build.sh to generate UTC timestamp
    The build.sh has been modified to generate UTC timestamp such
    that it is consistent across different time zones.

--- build.sh
+++ build.sh
@@ -226,7 +226,7 @@ if [ "$DEBUG" = true ] ; then
 fi
 
 if [ "$WITH_TIMESTAMP" = true ] ; then
-    TIMESTAMP="`date +"%Y%m%d%H%M%S"`"
+    TIMESTAMP="$(date -u +"%Y%m%d%H%M%S%Z")"
     _TIMESTAMP=".$TIMESTAMP"
 fi
 
