778928dadc30e9518c2ff82dc81a742d3cb64e51
Chris Kelley <ckelley@redhat.com>
2021-05-11 23:14:19 +0100

    Remove unused imports and tidy ones that remain

--- tomcat-9.0/src/main/java/org/dogtagpki/tomcat/JSSUtil.java
+++ tomcat-9.0/src/main/java/org/dogtagpki/tomcat/JSSUtil.java
@@ -21,29 +21,23 @@ package org.dogtagpki.tomcat;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.SSLEngine;
 
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
-import org.apache.tomcat.util.net.SSLHostConfigCertificate;
-import org.apache.tomcat.util.net.jsse.JSSEKeyManager;
 import org.apache.tomcat.util.net.SSLContext;
-import org.apache.tomcat.util.net.SSLUtil;
+import org.apache.tomcat.util.net.SSLHostConfigCertificate;
 import org.apache.tomcat.util.net.SSLUtilBase;
-
 import org.mozilla.jss.JSSProvider;
-import org.mozilla.jss.crypto.Policy;
 import org.mozilla.jss.provider.javax.crypto.JSSNativeTrustManager;
-import org.mozilla.jss.ssl.SSLCipher;
-import org.mozilla.jss.ssl.SSLVersion;
 
 public class JSSUtil extends SSLUtilBase {
     public static Log logger = LogFactory.getLog(JSSUtil.class);
